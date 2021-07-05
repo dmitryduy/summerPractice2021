01027fc7a7ab5513a1c906b1b6b5f8e304829d18
dmitryduy <cdcvh14@gmail.com>
2021-07-05 16:42:31 +0300

    add buttons amimations

--- source/src/main/kotlin/com/example/view/MainView.kt
+++ source/src/main/kotlin/com/example/view/MainView.kt
@@ -3,6 +3,7 @@ package com.example.view
 import com.example.graph.*
 import tornadofx.*
 import com.example.painter.*
+import javafx.scene.control.Button
 import javafx.scene.control.Label
 import javafx.scene.control.ScrollPane
 import javafx.scene.control.TableView
@@ -41,11 +42,22 @@ class MainView : View("Алгоритм Дейкстры") {
     private val currentActionText: Text by fxid("currentAction")
     private val currentOrderLabel: Label by fxid("currentOrder")
     private val tableContainer: ScrollPane by fxid("tableContainer")
+    private val rect: Button by fxid("rect")
+    private val rightButton: Button by fxid("rightButton")
+    private val leftButton: Button by fxid("leftButton")
+
 
     init {
         var g = Graph(graphType = GraphType.RandomGraph)
         var p = Painter()
-        root.add(p.paintGraph(g))
+
+        val graphPane = p.paintGraph(g)
+        graphPane.layoutY = 20.0
+        root.add(graphPane)
+
+        setButtonAnimation(leftButton, Pair(70.0, 51.0), Pair(306.0, 631.0), Pair(72.0, 53.0), Pair(305.0, 630.0))
+        setButtonAnimation(rightButton, Pair(70.0, 51.0), Pair(493.0, 631.0), Pair(72.0, 53.0), Pair(492.0, 630.0))
+        setButtonAnimation(rect, Pair(70.0, 51.0), Pair(398.0, 631.0), Pair(72.0, 53.0), Pair(397.0, 630.0))
 
         currentOrderLabel.text = "Очередь: ${order.joinToString(", ")}"
         currentActionText.text = currentAction;
@@ -72,6 +84,20 @@ class MainView : View("Алгоритм Дейкстры") {
                     isSortable = false
                 }
             }
+        }
+    }
+
+    private fun setButtonAnimation(button: Button, pressedSize: Pair<Double, Double>, pressedLayout: Pair<Double, Double>,
+                                   releasedSize: Pair<Double, Double>, releasedLayout: Pair<Double, Double>) {
+        button.setOnMousePressed {
+            button.setPrefSize(pressedSize.first, pressedSize.second)
+            button.layoutX = pressedLayout.first
+            button.layoutY = pressedLayout.second
+        }
+        button.setOnMouseReleased {
+            button.setPrefSize(releasedSize.first, releasedSize.second)
+            button.layoutX = releasedLayout.first
+            button.layoutY = releasedLayout.second
         }
     }
 }
