8bbcb56c590cfa2779142f92467df931b26a3720
dmitrikuz <54910797+dmitrikuz@users.noreply.github.com>
2021-07-06 03:32:01 +0300

    Added graph building by button press

--- source/src/main/kotlin/com/example/view/MainView.kt
+++ source/src/main/kotlin/com/example/view/MainView.kt
@@ -3,13 +3,11 @@ package com.example.view
 import com.example.graph.*
 import tornadofx.*
 import com.example.painter.*
-import javafx.scene.control.Label
-import javafx.scene.control.ScrollPane
-import javafx.scene.control.TableView
+import javafx.scene.control.*
 import javafx.scene.layout.AnchorPane
 import javafx.scene.layout.VBox
 import javafx.scene.text.Text
-
+import com.example.graphcontroller.*
 
 class Row(val array: Array<Any>)
 
@@ -41,11 +39,21 @@ class MainView : View("Алгоритм Дейкстры") {
     private val currentActionText: Text by fxid("currentAction")
     private val currentOrderLabel: Label by fxid("currentOrder")
     private val tableContainer: ScrollPane by fxid("tableContainer")
-
+    private val rect: Button by fxid("rect")
+    private val rightButton: Button by fxid("rightButton")
+    private val leftButton: Button by fxid("leftButton")
+    private val randomGraphBuild : MenuItem by fxid("randomGraphBuildButton")
+    private val buildGraphFromFileButton: MenuItem by fxid("buildGraphFromFileButton")
     init {
-        var g = Graph(graphType = GraphType.RandomGraph)
-        var p = Painter()
-        root.add(p.paintGraph(g))
+
+        var graphPane = pane()
+        var graph: Graph?
+        val p = Painter()
+        val gController =  GraphController()
+
+        setButtonAnimation(leftButton, Pair(70.0, 51.0), Pair(306.0, 631.0), Pair(72.0, 53.0), Pair(305.0, 630.0))
+        setButtonAnimation(rightButton, Pair(70.0, 51.0), Pair(493.0, 631.0), Pair(72.0, 53.0), Pair(492.0, 630.0))
+        setButtonAnimation(rect, Pair(70.0, 51.0), Pair(398.0, 631.0), Pair(72.0, 53.0), Pair(397.0, 630.0))
 
         currentOrderLabel.text = "Очередь: ${order.joinToString(", ")}"
         currentActionText.text = currentAction;
@@ -56,6 +64,25 @@ class MainView : View("Алгоритм Дейкстры") {
 
         tableContainer.add(createTable())
 
+        randomGraphBuild.setOnAction{
+
+            graph = Graph(graphType = GraphType.RandomGraph)
+            graphPane.clear()
+            val copy = graph!!
+            graphPane = p.paintGraph(copy)
+            graphPane.layoutY = 20.0
+            root.add(graphPane)
+        }
+        buildGraphFromFileButton.setOnAction{
+            graph = gController.buildFromFile()
+            if (graph != null) {
+                graphPane.clear()
+                val copy = graph!!
+                graphPane = p.paintGraph(copy)
+                graphPane.layoutY = 20.0
+                root.add(graphPane)
+            }
+        }
     }
 
     private fun createTable(): TableView<Row> {
@@ -72,6 +99,20 @@ class MainView : View("Алгоритм Дейкстры") {
                     isSortable = false
                 }
             }
+        }
+    }
+
+    private fun setButtonAnimation(button: Button, pressedSize: Pair<Double, Double>, pressedLayout: Pair<Double, Double>,
+                                   releasedSize: Pair<Double, Double>, releasedLayout: Pair<Double, Double>) {
+        button.setOnMousePressed {
+            button.setPrefSize(pressedSize.first, pressedSize.second)
+            button.layoutX = pressedLayout.first
+            button.layoutY = pressedLayout.second
+        }
+        button.setOnMouseReleased {
+            button.setPrefSize(releasedSize.first, releasedSize.second)
+            button.layoutX = releasedLayout.first
+            button.layoutY = releasedLayout.second
         }
     }
 }
