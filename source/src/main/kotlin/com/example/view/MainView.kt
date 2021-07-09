ca23d5dcc7e10210a6731be100eb59bdddc3a2aa
vikdema <viktordeminoff@gmail.com>
2021-07-09 15:52:34 +0300

    fix_bug

--- source/src/main/kotlin/com/example/view/MainView.kt
+++ source/src/main/kotlin/com/example/view/MainView.kt
@@ -73,13 +73,13 @@ class MainView : View("Алгоритм Дейкстры") {
         deleteVertexButton.setOnAction {
             graphController.state = GraphControllerState.DELETINGVERTEX
         }
-        addVertexButton.setOnAction{
+        addVertexButton.setOnAction {
             graphController.state = GraphControllerState.ADDINGVERTEX
         }
-        addEdgeButton.setOnAction{
+        addEdgeButton.setOnAction {
             graphController.state = GraphControllerState.CHOOSINGFIRSTVERTEX
         }
-        deleteEdgeButton.setOnAction{
+        deleteEdgeButton.setOnAction {
             graphController.state = GraphControllerState.DELETINGEDGE
         }
 
@@ -151,10 +151,10 @@ class MainView : View("Алгоритм Дейкстры") {
 
         startByStep.setOnAction {
             setGraphError.isVisible = !graphController.graphIsSet
-            if (graphController.graph != null && graphController.graphIsSet){
+            if (graphController.graph != null && graphController.graphIsSet) {
                 val d = Dijkstra()
                 temp = d.makeAlgorithm(graphController.graph!!, graphController.graph!!.getVertices()[0])
-                graphController.graph!!.getVertices().forEach {vertexes.add(it)}
+                graphController.graph!!.getVertices().forEach { vertexes.add(it) }
                 arrayPaths = getPaths(temp)
             }
             if (!isByStepStarted && graphController.graphIsSet) {
@@ -171,10 +171,10 @@ class MainView : View("Алгоритм Дейкстры") {
 
         autoplay.setOnAction {
             setGraphError.isVisible = !graphController.graphIsSet
-            if (graphController.graph != null && graphController.graphIsSet){
+            if (graphController.graph != null && graphController.graphIsSet) {
                 val d = Dijkstra()
                 temp = d.makeAlgorithm(graphController.graph!!, graphController.graph!!.getVertices()[0])
-                graphController.graph!!.getVertices().forEach {vertexes.add(it)}
+                graphController.graph!!.getVertices().forEach { vertexes.add(it) }
                 arrayPaths = getPaths(temp)
             }
             if (!isAutoplayStarted && graphController.graphIsSet) {
@@ -210,7 +210,7 @@ class MainView : View("Алгоритм Дейкстры") {
             buildGraph(graphController)
         }
 
-        saveToFileButton.setOnAction{
+        saveToFileButton.setOnAction {
             if (graphController.graph != null && graphController.graph?.getVertices()?.size != 0)
                 graphController.saveToFile()
         }
@@ -390,27 +390,26 @@ class MainView : View("Алгоритм Дейкстры") {
 
     }
 
-    private fun updateVertex(currentStepInfo: DijkstraStep){
+    private fun updateVertex(currentStepInfo: DijkstraStep) {
         updateEveryStep(currentStepInfo)
         val currentVertex = currentStepInfo.getCurrVertex()
         // обновляется текущее действие
-		
-		//------подсветка текущей вершины и ее соседей
+
+        //------подсветка текущей вершины и ее соседей
         var noQueueList = mutableListOf<VisualisedEdge>()
         var inQueueList = mutableListOf<VisualisedEdge>()
         var queueList = mutableListOf<Vertex>()
         graphController.restoreEdgesStyles()
         graphController.restoreVerticesStyle()
-        currentStepInfo.getQueue().forEach{
+        currentStepInfo.getQueue().forEach {
             queueList.add(it.first)
         }
-        graphController.visualEdges.forEach{
-            if (it.edge.first.getValue() == currentVertex.getValue()){
-                if (it.edge.second in queueList){
+        graphController.visualEdges.forEach {
+            if (it.edge.first.getValue() == currentVertex.getValue()) {
+                if (it.edge.second in queueList) {
 
                     inQueueList.add(it)
-                }
-                else{
+                } else {
                     noQueueList.add(it)
                 }
             }
@@ -418,35 +417,36 @@ class MainView : View("Алгоритм Дейкстры") {
         graphController.highlightVertices(listOf(graphController.getVisualVertex(currentVertex)!!), Color.LIGHTGREEN)
         graphController.highlightEdges(inQueueList, Color.LIGHTGREEN, Color.DARKGREEN)
         graphController.highlightEdges(noQueueList, Color.LIGHTGRAY, Color.DARKGRAY)
-		//----------
+        //----------
     }
 
-    private fun updatePath(currentStepInfo: DijkstraStep){
+    private fun updatePath(currentStepInfo: DijkstraStep) {
         updateEveryStep(currentStepInfo)
         // обновляется текущее действие
-        currentActionText.text += arrayPaths[countPaths-1]
-		
+        if (countPaths != 0) {
+            currentActionText.text += arrayPaths[countPaths - 1]
+        }
         //-----подсветка ребер и вершин входящих в найденный путь
         graphController.restoreVerticesStyle()
         graphController.restoreEdgesStyles()
         var eList = mutableListOf<VisualisedEdge>()
         var vList = mutableListOf<Vertex>()
         var initial = currentStepInfo.getPath(currentStepInfo.getCurrVertex()).split("->")
-        initial.forEach{
-                val v = graphController.graph!!.getVertex(it.substringBeforeLast("="))
-                if (v != null)
+        initial.forEach {
+            val v = graphController.graph!!.getVertex(it.substringBeforeLast("="))
+            if (v != null)
                 vList.add(v)
         }
-            for (h in 0 until vList.size - 1){
+        for (h in 0 until vList.size - 1) {
             eList.add(graphController.getVisualEdge(vList[h], vList[h + 1])!!)
         }
         val vvList = mutableListOf<VisualisedVertex>()
-        vList.forEach{
+        vList.forEach {
             vvList.add(graphController.getVisualVertex(it)!!)
         }
         graphController.highlightEdges(eList, Color.GREEN, Color.DARKGREEN)
         graphController.highlightVertices(vvList, Color.GREEN)
-		//--------
+        //--------
     }
 
     private fun updateQueue(currentStepInfo: DijkstraStep) {
@@ -464,12 +464,11 @@ class MainView : View("Алгоритм Дейкстры") {
     private fun buildGraph(controller: GraphController) {
 
         val graph = controller.graph
-        if (graph != null){
+        if (graph != null) {
             val graphPane = controller.wholePane
             graphPane.layoutY = 30.0
             root.add(graphPane)
-        }
-        else {
+        } else {
             graphController.graphIsSet = false
             setGraphError.isVisible = true
         }
